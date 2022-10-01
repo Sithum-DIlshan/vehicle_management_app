b81002a3faa7af23d3c76623a6901961144eb4d8
Sithum-DIlshan <sithumdilshan985@gmail.com>
2022-10-01 22:46:53 +0530

    use camera and gallery to choose photos done

--- screens/AddVehicle.js
+++ screens/AddVehicle.js
@@ -4,8 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faAt } from '@fortawesome/free-solid-svg-icons/faAt'
 import { faLock, faPhone, faUpload, faUserTie } from '@fortawesome/free-solid-svg-icons';
 import { normalize } from '../function/responsiveText';
-import { Actionsheet, Box, Button, HStack, Image, Input, Text, useDisclose, VStack } from 'native-base';
+import { Actionsheet, Box, Button, Divider, HStack, Image, Input, Text, useDisclose, VStack } from 'native-base';
 import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import Files from '../assets/files.png';
+import Camera from '../assets/camera.png';
 
 
 export default function AddVehicle() {
@@ -22,15 +24,24 @@ export default function AddVehicle() {
 
     const openCamera = async () => {
         const result = await launchCamera(options);
-        setImg(result.assets[0].path)
+
+        if(!result.didCancel){
+            images.push(result.assets[0].base64)
+            setImageLoad(true);
+        }
+        // setImg(result.assets[0].path)
     }
 
     const openGallery = async () => {
         const result = await launchImageLibrary(options);
-        result.assets.map(rest=>{
-           images.push(rest.base64);
-        })
-        setImageLoad(true);
+       
+        if(!result.didCancel){
+            result.assets.map(rest => {
+                images.push(rest.base64);
+            })
+            setImageLoad(true);
+        }
+        
     }
 
     const {
@@ -44,27 +55,24 @@ export default function AddVehicle() {
         <View style={styles.view} justifyContent={'flex-start'} alignItems={'center'}>
             <VStack mt={3} space={'2'} justifyContent="flex-start" alignItems={'center'} style={styles.inputAreaInner}>
                 <Text color={'#223555'} fontWeight={'bold'} fontSize={normalize(20)}>Add Vehicle</Text>
-                <Input InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faAt} style={styles.icon} />} style={styles.input} size={normalize(13)} variant="rounded" placeholder="Email ID" />
-                <Input InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faUserTie} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Full name" />
-                <Input InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faPhone} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Mobile" />
-                <Input type='password' InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faLock} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Password" />
-                <Input type='password' InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faLock} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Password" />
+                <Input backgroundColor={'white'} InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faAt} style={styles.icon} />} style={styles.input} size={normalize(13)} variant="rounded" placeholder="Email ID" />
+                <Input backgroundColor={'white'} InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faUserTie} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Full name" />
+                <Input backgroundColor={'white'} InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faPhone} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Mobile" />
+                <Input backgroundColor={'white'} type='password' InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faLock} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Password" />
+                <Input backgroundColor={'white'} type='password' InputLeftElement={<FontAwesomeIcon size={normalize(13)} color='#acb4c0' icon={faLock} style={styles.icon} />} size={normalize(13)} variant="rounded" placeholder="Password" />
                 <Button mt={3} bg={'#585e68'} startIcon={<FontAwesomeIcon size={normalize(13)} color='#f4f5f7' icon={faUpload} />} variant="outline" onPress={onOpen}><Text color={'#f4f5f7'}>Add Photos</Text></Button>
-                
+
             </VStack>
             <HStack>
-                
-                <Button>Hey</Button>
                 {/* <Image size={"md"} resizeMode="contain" source={{uri:'data:image/png;base64,'}} alt={"Alternate Text "} /> */}
-                {imageLoad && <Image size={"md"} resizeMode="contain" source={{uri:'data:image/png;base64,'+images[0]}} alt={"Alternate Text "} />}
-                <Button>Hey</Button>
-                <Button>Hey</Button>
-                <Button>Hey</Button>
+                {imageLoad && images.map(image => <Image key={image} size={"lg"} resizeMode="contain" source={{ uri: 'data:image/png;base64,' + image }} alt={"Alternate Text "} />)}
             </HStack>
             <Actionsheet isOpen={isOpen} onClose={onClose}>
                 <Actionsheet.Content>
-                    <Actionsheet.Item onPress={openCamera}>Camera</Actionsheet.Item>
-                    <Actionsheet.Item onPress={openGallery}>Upload</Actionsheet.Item>
+                    <Actionsheet.Item alignItems={'center'} startIcon={<Image size={normalize(27)} source={Camera} alt={""} />} onPress={openCamera}>Camera</Actionsheet.Item>
+                    <Divider my={1} width={'70%'} />
+
+                    <Actionsheet.Item alignItems={'center'} startIcon={<Image size={normalize(27)} source={Files} alt={""} />} onPress={openGallery}>Open</Actionsheet.Item>
                 </Actionsheet.Content>
             </Actionsheet>
         </View>
--- screens/navigator/BottomNavigator.js
+++ screens/navigator/BottomNavigator.js
@@ -4,14 +4,23 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddVehicle from '../AddVehicle';
 import ViewVehicle from '../ViewVehicle';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCab, faLock, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { normalize } from '../../function/responsiveText';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { color } from 'native-base/lib/typescript/theme/styled-system';
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function BottomNavigator() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen options={{ headerShown: false }}  name="AddVehicle" component={AddVehicle} />
-            <Tab.Screen options={{ headerShown: false }}  name="View" component={ViewVehicle} />
+        <Tab.Navigator initialRouteName="AddVehicle"
+            activeColor="#ffff"
+            inactiveColor="#6f93cc"
+            barStyle={{ backgroundColor: '#275db3' }}>
+            <Tab.Screen options={{ tabBarLabel:"Add",headerShown: false, tabBarIcon: () => (<FontAwesomeIcon size={normalize(15)} color='#acb4c0' icon={faPlus} />), }} name="AddVehicle" component={AddVehicle} />
+            <Tab.Screen options={{ headerShown: false,tabBarIcon: () => (<FontAwesomeIcon size={normalize(15)} color='#acb4c0' icon={faCab} />), }} name="View" component={ViewVehicle} />
         </Tab.Navigator>
     )
 }
